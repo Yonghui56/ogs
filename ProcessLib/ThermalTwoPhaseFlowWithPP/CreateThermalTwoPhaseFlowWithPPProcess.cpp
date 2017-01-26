632ac0e0b29f4b7c7165dcf47c33901cc79fec36
Yonghui <huangyh56@gmail.com>
2017-01-26 12:08:32 +0100

    create Thermal two phase flow PP model

---
+++ ProcessLib/ThermalTwoPhaseFlowWithPP/CreateThermalTwoPhaseFlowWithPPProcess.cpp
@@ -0,0 +1,194 @@
+/**
+ * \copyright
+ * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+#include "CreateThermalTwoPhaseFlowWithPPProcess.h"
+#include <cassert>
+#include "BaseLib/Functional.h"
+#include "MeshLib/MeshGenerators/MeshGenerator.h"
+#include "ProcessLib/Parameter/ConstantParameter.h"
+#include "ProcessLib/ThermalTwoPhaseFlowWithPP/CreateThermalTwoPhaseFlowWithPPMaterialProperties.h"
+#include "ProcessLib/ThermalTwoPhaseFlowWithPP/ThermalTwoPhaseFlowWithPPMaterialProperties.h"
+#include "ProcessLib/Utils/ParseSecondaryVariables.h"
+#include "ProcessLib/Utils/ProcessUtils.h"
+#include "ThermalTwoPhaseFlowWithPPProcess.h"
+#include "ThermalTwoPhaseFlowWithPPProcessData.h"
+
+namespace ProcessLib
+{
+namespace ThermalTwoPhaseFlowWithPP
+{
+std::unique_ptr<Process> createThermalTwoPhaseFlowWithPPProcess(
+    MeshLib::Mesh& mesh,
+    std::unique_ptr<ProcessLib::AbstractJacobianAssembler>&& jacobian_assembler,
+    std::vector<ProcessVariable> const& variables,
+    std::vector<std::unique_ptr<ParameterBase>> const& parameters,
+    unsigned const integration_order,
+    BaseLib::ConfigTree const& config,
+    std::map<std::string,
+             std::unique_ptr<MathLib::PiecewiseLinearInterpolation>> const&
+        curves)
+{
+    //! \ogs_file_param{prj__processes__process__type}
+    config.checkConfigParameter("type", "THERMAL_TWOPHASE_WITH_PP");
+
+    DBUG("Create TwoPhaseFlowProcess with Prho model.");
+    //! \ogs_file_param{prj__processes__process__TWOPHASE_FLOW_PRHO__process_variables}
+    auto const pv_config = config.getConfigSubtree("process_variables");
+
+    auto process_variables = findProcessVariables(
+        variables, pv_config,
+        {//! \ogs_file_param_special{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__process_variables__gas_pressure}
+         "gas_pressure",
+         //! \ogs_file_param_special{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__process_variables__capillary_pressure}
+         "capillary_pressure",
+         //! \ogs_file_param_special{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__process_variables__temperature}
+         "temperature"});
+
+    SecondaryVariableCollection secondary_variables;
+
+    NumLib::NamedFunctionCaller named_function_caller(
+        {"TwoPhaseFlow_pressure"});
+
+    ProcessLib::parseSecondaryVariables(config, secondary_variables,
+                                        named_function_caller);
+    // Specific body force
+    std::vector<double> const b =
+        //! \ogs_file_param{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__specific_body_force}
+        config.getConfigParameter<std::vector<double>>("specific_body_force");
+    assert(b.size() > 0 && b.size() < 4);
+    Eigen::VectorXd specific_body_force(b.size());
+    bool const has_gravity = MathLib::toVector(b).norm() > 0;
+    if (has_gravity)
+        std::copy_n(b.data(), b.size(), specific_body_force.data());
+
+    //! \ogs_file_param{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__mass_lumping}
+    auto mass_lumping = config.getConfigParameter<bool>("mass_lumping");
+    // diffusion coeff
+    auto& diff_coeff_b = findParameter<double>(
+        config,
+        //! \ogs_file_param{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__diffusion_coeff_component_b}
+        "diffusion_coeff_component_b", parameters, 1);
+    auto& diff_coeff_a = findParameter<double>(
+        config,
+        //! \ogs_file_param{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__diffusion_coeff_component_a}
+        "diffusion_coeff_component_a", parameters, 1);
+
+    // Parameter for the specific heat capacity of the solid.
+    auto& specific_heat_capacity_solid = findParameter<double>(
+        config,
+        //! \ogs_file_param{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__specific_heat_capacity_solid}
+        "specific_heat_capacity_solid", parameters, 1);
+    DBUG("Use \'%s\' as specific_heat_capacity_solid parameter.",
+         specific_heat_capacity_solid.name.c_str());
+
+    // Parameter for the specific heat capacity of the solid.
+    auto& specific_heat_capacity_water = findParameter<double>(
+        config,
+        //! \ogs_file_param{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__specific_heat_capacity_water}
+        "specific_heat_capacity_water", parameters, 1);
+    DBUG("Use \'%s\' as specific_heat_capacity_water parameter.",
+         specific_heat_capacity_water.name.c_str());
+
+    // Parameter for the specific heat capacity of the dry air.
+    auto& specific_heat_capacity_dry_air = findParameter<double>(
+        config,
+        //! \ogs_file_param{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__specific_heat_capacity_dry_air}
+        "specific_heat_capacity_dry_air", parameters, 1);
+    DBUG("Use \'%s\' as specific_heat_capacity_dry_air parameter.",
+         specific_heat_capacity_dry_air.name.c_str());
+
+    // Parameter for the specific heat capacity of the water vapor.
+    auto& specific_heat_capacity_water_vapor = findParameter<double>(
+        config,
+        //! \ogs_file_param{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__specific_heat_capacity_water_vapor}
+        "specific_heat_capacity_water_vapor", parameters, 1);
+    DBUG("Use \'%s\' as specific_heat_capacity_water_vapor parameter.",
+         specific_heat_capacity_water_vapor.name.c_str());
+
+    // Parameter for the heat conductivity of the dry solid
+    auto& heat_conductivity_dry_solid = findParameter<double>(
+        config,
+        //! \ogs_file_param{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__heat_conductivity_dry_solid}
+        "heat_conductivity_dry_solid", parameters, 1);
+    DBUG("Use \'%s\' as heat_conductivity_dry_solid parameter.",
+        heat_conductivity_dry_solid.name.c_str());
+
+    // Parameter for the heat conductivity of the wet solid
+    auto& heat_conductivity_wet_solid = findParameter<double>(
+        config,
+        //! \ogs_file_param{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__heat_conductivity_wet_solid}
+        "heat_conductivity_wet_solid", parameters, 1);
+    DBUG("Use \'%s\' as heat_conductivity_wet_solid parameter.",
+        heat_conductivity_wet_solid.name.c_str());
+
+    // Parameter for the density of the solid.
+
+    auto& density_solid = findParameter<double>(
+        config,
+        //! \ogs_file_param_special{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__density_solid}
+        "density_solid", parameters, 1);
+    DBUG("Use \'%s\' as density_solid parameter.", density_solid.name.c_str());
+
+    // Parameter for the latent heat of evaporation.
+    auto& latent_heat_evaporation = findParameter<double>(
+        config,
+        //! \ogs_file_param{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__latent_heat_evaporation}
+        "latent_heat_evaporation", parameters, 1);
+    DBUG("Use \'%s\' as latent_heat_evaporation parameter.",
+         latent_heat_evaporation.name.c_str());
+
+    //! \ogs_file_param{prj__processes__process__THERMAL_TWOPHASE_FLOW_PP__material_property}
+    auto const& mat_config = config.getConfigSubtree("material_property");
+
+    auto const& mat_ids =
+        mesh.getProperties().getPropertyVector<int>("MaterialIDs");
+
+    std::unique_ptr<ProcessLib::ThermalTwoPhaseFlowWithPP::
+                        ThermalTwoPhaseFlowWithPPMaterialProperties>
+        material = nullptr;
+
+    boost::optional<MeshLib::PropertyVector<int> const&> material_ids;
+
+    if (mat_ids != nullptr)
+    {
+        INFO("The twophase flow is in heterogeneous porous media.");
+        material_ids = *mat_ids;
+    }
+    else
+    {
+        INFO("The twophase flow is in homogeneous porous media.");
+    }
+
+    material = ProcessLib::ThermalTwoPhaseFlowWithPP::createThermalTwoPhaseFlowWithPPMaterialProperties(mat_config, material_ids);
+
+    ThermalTwoPhaseFlowWithPPProcessData process_data{
+        specific_body_force,
+        has_gravity,
+        mass_lumping,
+        diff_coeff_b,
+        diff_coeff_a,
+        specific_heat_capacity_solid,
+        specific_heat_capacity_water,
+        specific_heat_capacity_dry_air,
+        specific_heat_capacity_water_vapor,
+        heat_conductivity_dry_solid,
+        heat_conductivity_wet_solid,
+        density_solid,
+        latent_heat_evaporation,
+        std::move(material) };
+
+    return std::unique_ptr<Process>{new ThermalTwoPhaseFlowWithPPProcess{
+        mesh, std::move(jacobian_assembler), parameters, integration_order,
+        std::move(process_variables), std::move(process_data),
+        std::move(secondary_variables), std::move(named_function_caller),
+        mat_config, curves }};
+   
+}
+
+}  // end of namespace
+}  // end of namespace
---
+++ ProcessLib/ThermalTwoPhaseFlowWithPP/ThermalTwoPhaseFlowWithPPMaterialProperties.cpp
@@ -0,0 +1,275 @@
+/**
+* \copyright
+* Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+*            Distributed under a Modified BSD License.
+*              See accompanying file LICENSE.txt or
+*              http://www.opengeosys.org/project/license
+*
+*/
+
+#include "ThermalTwoPhaseFlowWithPPMaterialProperties.h"
+#include <logog/include/logog.hpp>
+#include "BaseLib/reorderVector.h"
+#include "MaterialLib/Fluid/FluidProperty.h"
+#include "MaterialLib/PorousMedium/Porosity/Porosity.h"
+#include "MaterialLib/PorousMedium/Storage/Storage.h"
+#include "MaterialLib/PorousMedium/UnsaturatedProperty/CapillaryPressure/CapillaryPressureSaturation.h"
+#include "MaterialLib/PorousMedium/UnsaturatedProperty/CapillaryPressure/CreateCapillaryPressureModel.h"
+#include "MaterialLib/PorousMedium/UnsaturatedProperty/RelativePermeability/CreateRelativePermeabilityModel.h"
+#include "MaterialLib/PorousMedium/UnsaturatedProperty/RelativePermeability/RelativePermeability.h"
+#include "MathLib/InterpolationAlgorithms/PiecewiseLinearInterpolation.h"
+#include "MeshLib/Mesh.h"
+#include "MeshLib/PropertyVector.h"
+#include "NumLib/NewtonRaphson.h"
+#include "ProcessLib/Parameter/Parameter.h"
+#include "ProcessLib/Parameter/SpatialPosition.h"
+
+/**
+* Regarding the details of the material propertiy functions
+* please refer to: Class H, Helmig R, Bastian P. Numerical simulation of
+* non- isothermal multiphase multicomponent processes in Porous Media¨C¨C1. An
+* efficient solution technique. Adv Water Resour, 2002.
+*/
+
+using MaterialLib::PhysicalConstant::MolarMass::Water;
+using MaterialLib::PhysicalConstant::MolarMass::Air;
+using MaterialLib::PhysicalConstant::IdealGasConstant;
+namespace ProcessLib
+{
+namespace ThermalTwoPhaseFlowWithPP
+{
+ThermalTwoPhaseFlowWithPPMaterialProperties::
+    ThermalTwoPhaseFlowWithPPMaterialProperties(
+        boost::optional<MeshLib::PropertyVector<int> const&> const material_ids,
+        std::unique_ptr<MaterialLib::Fluid::FluidProperty>
+            liquid_density,
+        std::unique_ptr<MaterialLib::Fluid::FluidProperty>
+            viscosity,
+        std::unique_ptr<MaterialLib::Fluid::FluidProperty>
+            gas_density,
+        std::unique_ptr<MaterialLib::Fluid::FluidProperty>
+            gas_viscosity,
+        std::vector<Eigen::MatrixXd>
+            intrinsic_permeability_models,
+        std::vector<std::unique_ptr<MaterialLib::PorousMedium::Porosity>>&&
+            porosity_models,
+        std::vector<std::unique_ptr<MaterialLib::PorousMedium::Storage>>&&
+            storage_models,
+        std::vector<std::unique_ptr<
+            MaterialLib::PorousMedium::CapillaryPressureSaturation>>&&
+            capillary_pressure_models,
+        std::vector<
+            std::unique_ptr<MaterialLib::PorousMedium::RelativePermeability>>&&
+            relative_permeability_models)
+    : _liquid_density(std::move(liquid_density)),
+      _viscosity(std::move(viscosity)),
+      _gas_density(std::move(gas_density)),
+      _gas_viscosity(std::move(gas_viscosity)),
+      _material_ids(material_ids),
+      _intrinsic_permeability_models(intrinsic_permeability_models),
+      _porosity_models(std::move(porosity_models)),
+      _storage_models(std::move(storage_models)),
+      _capillary_pressure_models(std::move(capillary_pressure_models)),
+      _relative_permeability_models(std::move(relative_permeability_models))
+{
+    DBUG("Create material properties for Two-Phase flow with Prho model.");
+}
+
+int ThermalTwoPhaseFlowWithPPMaterialProperties::getMaterialID(
+    const std::size_t element_id)
+{
+    if (!_material_ids)
+    {
+        return 0;
+    }
+
+    assert(element_id < _material_ids->size());
+    return (*_material_ids)[element_id];
+}
+
+double ThermalTwoPhaseFlowWithPPMaterialProperties::getLiquidDensity(
+    const double p, const double T) const
+{
+    ArrayType vars;
+    vars[static_cast<int>(MaterialLib::Fluid::PropertyVariableType::T)] = T;
+    vars[static_cast<int>(MaterialLib::Fluid::PropertyVariableType::p)] = p;
+    return _liquid_density->getValue(vars);
+}
+
+double ThermalTwoPhaseFlowWithPPMaterialProperties::getGasDensity(
+    const double p, const double T) const
+{
+    ArrayType vars;
+    vars[static_cast<int>(MaterialLib::Fluid::PropertyVariableType::T)] = T;
+    vars[static_cast<int>(MaterialLib::Fluid::PropertyVariableType::p)] = p;
+    return _gas_density->getValue(vars);
+}
+
+double ThermalTwoPhaseFlowWithPPMaterialProperties::getDerivGasDensity(
+    const double p, const double T) const
+{
+    ArrayType vars;
+    vars[static_cast<int>(MaterialLib::Fluid::PropertyVariableType::T)] = T;
+    vars[static_cast<int>(MaterialLib::Fluid::PropertyVariableType::p)] = p;
+
+    return _gas_density->getdValue(vars,
+                                   MaterialLib::Fluid::PropertyVariableType::p);
+}
+double ThermalTwoPhaseFlowWithPPMaterialProperties::getLiquidViscosity(
+    const double p, const double T) const
+{
+    ArrayType vars;
+    vars[static_cast<int>(MaterialLib::Fluid::PropertyVariableType::T)] = T;
+    vars[static_cast<int>(MaterialLib::Fluid::PropertyVariableType::p)] = p;
+    return _viscosity->getValue(vars);
+}
+
+double ThermalTwoPhaseFlowWithPPMaterialProperties::getGasViscosity(
+    const double p, const double T) const
+{
+    ArrayType vars;
+    vars[static_cast<int>(MaterialLib::Fluid::PropertyVariableType::T)] = T;
+    vars[static_cast<int>(MaterialLib::Fluid::PropertyVariableType::p)] = p;
+    return _gas_viscosity->getValue(vars);
+}
+
+Eigen::MatrixXd const&
+ThermalTwoPhaseFlowWithPPMaterialProperties::getPermeability(
+    const double /*t*/, const ProcessLib::SpatialPosition& /*pos*/,
+    const int /*dim*/) const
+{
+    return _intrinsic_permeability_models[_current_material_id];
+}
+
+double ThermalTwoPhaseFlowWithPPMaterialProperties::getPorosity(
+    const double /*t*/, const ProcessLib::SpatialPosition& /*pos*/,
+    const double /*p*/, const double T, const double porosity_variable) const
+{
+    const double porosity =
+        _porosity_models[_current_material_id]->getValue(porosity_variable, T);
+
+    return porosity;
+}
+
+double
+ThermalTwoPhaseFlowWithPPMaterialProperties::getNonwetRelativePermeability(
+    const double /*t*/, const ProcessLib::SpatialPosition& /*pos*/,
+    const double /*p*/, const double /*T*/, const double saturation) const
+{
+    /*const double nonwet_krel =
+        _relative_permeability_models[0]->getValue(saturation);
+    return nonwet_krel;*/
+    const double Se = (saturation - 0.15) / (1 - 0.15);
+    if (saturation < 0.15)
+        return 1.0;
+    return pow(1 - Se, 3);
+}
+
+double ThermalTwoPhaseFlowWithPPMaterialProperties::getWetRelativePermeability(
+    const double /*t*/, const ProcessLib::SpatialPosition& /*pos*/,
+    const double /*p*/, const double /*T*/, const double saturation) const
+{
+    /*const double wet_krel =
+        _relative_permeability_models[1]->getValue(saturation);
+    return wet_krel;*/
+    const double Se = (saturation - 0.15) / (1 - 0.15);
+    if (Se < 0)
+        return 0.0;
+    return pow(Se, 3);
+}
+
+double ThermalTwoPhaseFlowWithPPMaterialProperties::getSaturation(
+    const double /*t*/, const ProcessLib::SpatialPosition& /*pos*/,
+    const double /*p*/, const double /*T*/, const double pc) const
+{
+    return _capillary_pressure_models[_current_material_id]->getSaturation(pc);
+}
+
+double ThermalTwoPhaseFlowWithPPMaterialProperties::getCapillaryPressure(
+    const double /*t*/, const ProcessLib::SpatialPosition& /*pos*/,
+    const double /*p*/, const double /*T*/, const double saturation) const
+{
+    return _capillary_pressure_models[_current_material_id]
+        ->getCapillaryPressure(saturation);
+}
+
+double ThermalTwoPhaseFlowWithPPMaterialProperties::getSaturationDerivative(
+    const double /*t*/, const ProcessLib::SpatialPosition& /*pos*/,
+    const double /*p*/, const double /*T*/, const double saturation) const
+{
+    const double dpcdsw =
+        _capillary_pressure_models[_current_material_id]->getdPcdS(saturation);
+    const double dswdpc = 1 / dpcdsw;
+    return dswdpc;
+}
+
+double
+ThermalTwoPhaseFlowWithPPMaterialProperties::calculateUnsatHeatConductivity(
+    double const /*t*/, ProcessLib::SpatialPosition const& /*x*/,
+    double const Sw, double const lambda_pm_dry,
+    double const lambda_pm_wet) const
+{
+    double lambda_pm =
+        lambda_pm_dry + std::pow(Sw, 0.5) * (lambda_pm_wet - lambda_pm_dry);
+    if (Sw > 1)
+        lambda_pm = lambda_pm_wet;
+    else if (Sw < 0)
+        lambda_pm = lambda_pm_dry;
+    return lambda_pm;
+}
+double
+ThermalTwoPhaseFlowWithPPMaterialProperties::calculateSaturatedVaporPressure(
+    const double T) const
+{
+    const double T_0 = 373.15;
+    const double p_0 = 101325.0;
+    const double h_wg = 2258000.0;
+    return p_0 * exp(((1 / T_0) - (1 / T)) * Water * h_wg / IdealGasConstant);
+}
+double
+ThermalTwoPhaseFlowWithPPMaterialProperties::calculateVaporPressureNonwet(
+    const double pc, const double T) const
+{
+    const double p_sat = calculateSaturatedVaporPressure(T);
+    const double C_w = Water / IdealGasConstant / T;
+    return p_sat * exp(-pc * C_w / rho_mass_h20);
+}
+double ThermalTwoPhaseFlowWithPPMaterialProperties::calculateDerivativedPsatdT(
+    const double T) const
+{
+    const double T_0 = 373.15;
+    const double p_0 = 101325;
+    const double h_wg = 2258000.0;
+    return p_0 * (Water * h_wg / IdealGasConstant) * (1. / T / T) *
+           exp(((1. / T_0) - (1. / T)) * Water * h_wg / IdealGasConstant);
+}
+double ThermalTwoPhaseFlowWithPPMaterialProperties::calculateDerivativedPgwdT(
+    const double pc, const double T) const
+{
+    const double C_w = Water / IdealGasConstant / T;
+    const double p_sat = calculateSaturatedVaporPressure(T);
+    const double dPsatdT = calculateDerivativedPsatdT(T);
+    return dPsatdT * exp(-pc * C_w / rho_mass_h20) +
+           p_sat * exp(-pc * C_w / rho_mass_h20) *
+               (pc * Water / rho_mass_h20 / IdealGasConstant / T / T);
+}
+double ThermalTwoPhaseFlowWithPPMaterialProperties::calculateDerivativedPgwdPC(
+    const double pc, const double T) const
+{
+    const double C_w = Water / IdealGasConstant / T;
+    const double p_sat = calculateSaturatedVaporPressure(T);
+    return p_sat * exp(-pc * C_w / rho_mass_h20) * (-C_w / rho_mass_h20);
+}
+double ThermalTwoPhaseFlowWithPPMaterialProperties::calculatedRhoNonwetdT(
+    const double p_air_nonwet, const double p_vapor_nonwet, const double PC,
+    const double T) const
+{
+    const double C_v = Air / IdealGasConstant / T;
+    const double dPgwdT = calculateDerivativedPgwdT(PC, T);
+    return -((p_air_nonwet * Air + p_vapor_nonwet * Water) / IdealGasConstant /
+             T / T) +
+           (Water - Air) * dPgwdT / IdealGasConstant / T;
+}
+
+}  // end of namespace
+}  // end of namespace
